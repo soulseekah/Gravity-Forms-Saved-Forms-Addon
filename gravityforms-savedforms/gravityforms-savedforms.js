595b69279923d46322878acd9f70ebcd393cff03
Gennady Kovshenin <gennady@kovshenin.com>
2014-08-31 15:13:22 +0600

    Fixed support for multiple save buttons
    See #24, where multiple save form buttons were inserted. The clone
    function, active in a multi-page setup, would clone all of them, instead
    of just one.

--- gravityforms-savedforms/gravityforms-savedforms.js
+++ gravityforms-savedforms/gravityforms-savedforms.js
@@ -1,4 +1,4 @@
 /* On multi-page forms the Save button should display on each page */
 jQuery( document ).bind( 'gform_post_render', function() {
-	jQuery( '.gform_next_button' ).after( jQuery( '.gform_save_state' ).clone() );
+	jQuery( '.gform_next_button' ).after( jQuery( '.gform_save_state:last' ).clone() );
 } );
