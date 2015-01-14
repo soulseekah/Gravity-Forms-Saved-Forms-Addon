229902159f552d0e98fb71eb3da5b8ea42bead35
Gennady Kovshenin <gennady@kovshenin.com>
2015-01-15 01:35:59 +0500

    Filter multiple save buttons for multipage forms
    Multiple forms on the same page triggered the addition of the save
    button several times, bringing chaos. Fixes #26. Note that there are
    still issues when saving, the saved form data is being overridden and is
    discussed in #27.

--- gravityforms-savedforms/gravityforms-savedforms.js
+++ gravityforms-savedforms/gravityforms-savedforms.js
@@ -1,4 +1,6 @@
 /* On multi-page forms the Save button should display on each page */
-jQuery( document ).bind( 'gform_post_render', function() {
-	jQuery( '.gform_next_button' ).after( jQuery( '.gform_save_state:last' ).clone() );
+jQuery( document ).bind( 'gform_post_render', function( e, form_id, page ) {
+	var save_button = jQuery( '#gform_' + form_id + ' .gform_save_state' );
+	if ( !save_button ) return;
+	save_button.parents( 'form' ).find( '.gform_next_button' ).after( save_button.clone() );
 } );
